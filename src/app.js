e7df9e656506c2d96f6a507545943e8e998912d9
Umasou <86976137+Umasou4765@users.noreply.github.com>
2025-06-28 00:35:50 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-console.log("JS is working!");
-
 document.getElementById('uploadForm').addEventListener('submit', function(e) {
   e.preventDefault();
   alert("🚧 Feature under development. Upload is currently unavailable.");
