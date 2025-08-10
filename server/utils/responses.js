147cad3343e1246e266312a455779f8090823f1f
Umasou <86976137+Umasou4765@users.noreply.github.com>
2025-08-11 01:02:07 +0800

    Delete server directory

--- server/utils/responses.js
+++
@@ -1,7 +0,0 @@
-function success(res, data = null, message = 'OK', status = 200) {
-  return res.status(status).json({ success: true, message, data });
-}
-function fail(res, message = 'Error', status = 400, code = null, extra = {}) {
-  return res.status(status).json({ success: false, error: { message, code, ...extra } });
-}
-module.exports = { success, fail };
--- server/utils/validators.js
+++
@@ -1,8 +0,0 @@
-function isValidEmail(email) {
-  return /\S+@\S+\.\S+/.test(email);
-}
-function passwordPolicy(pw) {
-  if (!pw || pw.length < 8) return 'Password must be at least 8 characters.';
-  return null;
-}
-module.exports = { isValidEmail, passwordPolicy };
